cee919ebe81bf6c2a0cedb77fb2f1a143974223a
Frank Rose <frankrosework@gmail.com>
2016-11-19 02:22:36 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"])# Write your #display_rainbow method here
+  #colors.each { |x| print "#{x.slice(0).capitalize}: #{x} " }
+  #puts
+  print "#{colors[0].slice(0).capitalize}: #{colors[0]}, "
+  print "#{colors[1].slice(0).capitalize}: #{colors[1]}, "
+  print "#{colors[2].slice(0).capitalize}: #{colors[2]}, "
+  print "#{colors[3].slice(0).capitalize}: #{colors[3]}, "
+  print "#{colors[4].slice(0).capitalize}: #{colors[4]}, "
+  print "#{colors[5].slice(0).capitalize}: #{colors[5]}, "
+  print "#{colors[6].slice(0).capitalize}: #{colors[6]}"
+  puts ""
+end
